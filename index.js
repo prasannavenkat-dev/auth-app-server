a8dedae67f8bfc59d2dcc3346836bd0039edaed8
unknown <prasannavenkat1998@yahoo.com>
2022-11-24 17:30:51 +0530

    Added cors header

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ const corsOptions = {
   origin: "*",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
+  "Access-Control-Allow-Origin": "*"
 };
 
 app.use(cors(corsOptions)); // Use this after the variable declaration
